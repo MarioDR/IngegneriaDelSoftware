5b8a0978840fabf10f1f5871f5a527a4f92f2488
francesco grimaldi <80365006+fragrimaldi5@users.noreply.github.com>
2025-11-18 14:54:33 +0100

    configurazione file pom.xml per i test junit

---
+++ IngegneriaDelSoftware/src/test/java/Test/MainTest.java
@@ -0,0 +1,21 @@
+package Test;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    public void primoTest() {
+        assertTrue(true);
+    }
+    
+    @Test
+    public void secondoTest() {
+        assertTrue(true);
+    }
+    
+    @Test
+    public void terzoTest() {
+        assertTrue(true);
+    }
+}
