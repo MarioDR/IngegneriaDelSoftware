d79f2cb6ea7ead5662a2f706d4a76d4e22d86178
MarioDR <mariodellorusso8@gmail.com>
2025-12-08 13:36:50 +0100

    Terzo commit doxygen

--- implementazione/src/main/java/gruppo15/ingegneriadelsoftware/model/exceptions/UtenteNonValidoException.java
+++ implementazione/src/main/java/gruppo15/ingegneriadelsoftware/model/exceptions/UtenteNonValidoException.java
@@ -1,11 +1,29 @@
 package gruppo15.ingegneriadelsoftware.model.exceptions;
 
+/**
+ * @file UtenteNonValidoException.java
+ * @brief Questo file contiene l'implementazione dell'eccezione UtenteNonValidoException
+ * 
+ * L'eccezione viene lanciata se l'utente risulta non essere valido(alcuni dei 
+ * campi inseriti per la registrazione dell'utente sono errati)
+ * 
+ * @see ValidationException
+ * @author Gruppo15
+ * @version 1.0
+ */
 public class UtenteNonValidoException extends ValidationException {
 
+    /**
+     * @brief Costruttore dell'eccezione senza parametro
+     */
     public UtenteNonValidoException() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * @brief Costruttore dell'eccezione con parametro
+     * @param[in] msg Il messaggio che sarà mostrato al lancio dell'eccezione 
+     */
     public String ModificaUtenteErrataException(String msg) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
