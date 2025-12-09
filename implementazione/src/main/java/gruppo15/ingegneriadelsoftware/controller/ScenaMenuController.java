85d2c51e05958c4cb55314149fb98da4d25f1929
francesco grimaldi <80365006+fragrimaldi5@users.noreply.github.com>
2025-12-10 00:52:49 +0100

    prime scene senza logica

---
+++ implementazione/src/main/java/gruppo15/ingegneriadelsoftware/controller/ScenaMenuController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gruppo15.ingegneriadelsoftware.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+/**
+ *
+ * @author fgrim
+ */
+public class ScenaMenuController {
+
+    @FXML
+    private void visualizzaListaUtenti(ActionEvent event) {
+    }
+
+    @FXML
+    private void aggiungiUtente(ActionEvent event) {
+    }
+
+    @FXML
+    private void visualizzaListaLibri(ActionEvent event) {
+    }
+
+    @FXML
+    private void aggiungiLibro(ActionEvent event) {
+    }
+
+    @FXML
+    private void visualizzaListaPrestiti(ActionEvent event) {
+    }
+
+    @FXML
+    private void aggiungiPrestito(ActionEvent event) {
+    }
+
+    @FXML
+    private void visualizzaRestituzioni(ActionEvent event) {
+    }
+
+    @FXML
+    private void visualizzaStatistiche(ActionEvent event) {
+    }
+    
+}
