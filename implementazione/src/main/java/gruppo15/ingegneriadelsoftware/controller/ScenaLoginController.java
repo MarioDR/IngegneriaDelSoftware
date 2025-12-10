f2a9485eca63a95ead2d61071bafd60a5ec7ce9a
francesco grimaldi <80365006+fragrimaldi5@users.noreply.github.com>
2025-12-10 02:55:40 +0100

    prima implementazione del login - prova

--- implementazione/src/main/java/gruppo15/ingegneriadelsoftware/controller/ScenaLoginController.java
+++ implementazione/src/main/java/gruppo15/ingegneriadelsoftware/controller/ScenaLoginController.java
@@ -5,11 +5,14 @@
  */
 package gruppo15.ingegneriadelsoftware.controller;
 
+import gruppo15.ingegneriadelsoftware.view.App;
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -18,16 +21,40 @@ import javafx.scene.control.TextField;
 public class ScenaLoginController {
 
     @FXML
-    private Button loginButton;
-    @FXML
     private TextField usernameField;
     @FXML
     private TextField passwordField;
     @FXML
     private Label labelErroreLogin;
+    @FXML
+    private Button loginButtonAccedi;
 
     @FXML
     private void clickAccedi(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        // ----------------------------------------------------
+        // 1. Validazione (Logica Fittizia)
+        // ----------------------------------------------------
+        if (username.equals("biblio") && password.equals("pass")) {
+            
+            // 2. Cambio di Scena
+            try {
+                // Chiama il metodo statico per caricare ScenaMenu.fxml
+                // e sostituire la radice della Scene corrente.
+                App.setRoot("ScenaMenu"); 
+                
+            } catch (IOException e) {
+                // Gestione dell'errore nel caso il file ScenaMenu.fxml non venga trovato
+                e.printStackTrace();
+                labelErroreLogin.setText("Errore nel caricamento della scena principale. Controllare i file FXML.");
+            }
+            
+        } else {
+            // Credenziali errate
+            labelErroreLogin.setText("❌ Credenziali non valide. Riprovare.");
+        }
     }
     
 }
